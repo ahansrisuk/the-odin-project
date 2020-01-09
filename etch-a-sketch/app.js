6b3c1b9c7fc92c5bf254d9afd4b16117b4f8def5
Allen Hansrisuk <ahansrisuk@email.com>
2020-01-09 12:48:21 -0500

    instantiate box

--- etch-a-sketch/app.js
+++ etch-a-sketch/app.js
@@ -1,1 +1,14 @@
 console.log("Hello World")
+
+const gridContainer = document.querySelector(".container");
+
+    for (let i = 0; i < 4; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
+        gridContainer.appendChild(gridRow);
+        for (let i = 0; i < 4; i++) {
+            const box = document.createElement('div');
+            box.classList.add('box')
+            gridRow.appendChild(box);
+        }
+    }
